af7759c63584c3b2cb18eea124737fbfca9d4fee
Viktor <viktor@Viktors-MacBook-Pro.local>
2021-12-12 20:28:54 +0200

    myFirstCommit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import {Header} from "./components/Header/Header";
+import {DashboardPage} from "./pages/DashboardPage/DashboardPage";
+
+function App() {
+  return (
+    <div>
+      <DashboardPage/>
+    </div>
+  );
+}
+
+export default App;
